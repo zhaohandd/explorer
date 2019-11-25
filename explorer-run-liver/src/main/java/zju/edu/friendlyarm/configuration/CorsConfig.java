a2d8068473c4dfbaaebe4cf829fd702e7e316ba9
xhzhao <xhzhao@zju.edu.cn>
2019-11-25 19:51:03 +0800

    解决跨域问题

---
+++ explorer-run-liver/src/main/java/zju/edu/friendlyarm/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package zju.edu.friendlyarm.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author xhzhao
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        //添加映射路径
+        corsRegistry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOrigins("*")
+                //是否发送Cookie信息
+                .allowCredentials(true)
+                //放行哪些原始域(请求方式)
+                .allowedMethods("GET","POST", "PUT", "DELETE")
+                //放行哪些原始域(头部信息)
+                .allowedHeaders("*")
+                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                .exposedHeaders("Header1", "Header2");
+    }
+}
--- explorer-run-liver/src/main/java/zju/edu/friendlyarm/controller/ImageController.java
+++ explorer-run-liver/src/main/java/zju/edu/friendlyarm/controller/ImageController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
